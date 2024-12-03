4c5700be56069546708981db32c13394de62e637
Kamalov Kamil <kamil17121998@gmail.com>
2024-12-03 12:36:43 +0300

    implement repository

--- go.mod
+++ go.mod
@@ -11,12 +11,15 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/lallison21/auth_service/internal/models"
+)
 
 type Repository struct {
 	db *pgxpool.Pool
@@ -10,4 +15,44 @@ func New(db *pgxpool.Pool) *Repository {
 	return &Repository{
 		db: db,
 	}
+}
+
+func (r *Repository) Register(ctx context.Context, newUser *models.UserDao) (int, error) {
+	query := `INSERT INTO users(username, password, password) VALUES ($1, $2, $3) RETURNING id`
+
+	var userId int
+	err := r.db.QueryRow(ctx, query, newUser.Username, newUser.Password, newUser.Password).Scan(&userId)
+	if err != nil {
+		return -1, err
+	}
+
+	return userId, nil
+}
+
+func (r *Repository) GetUserByUsernameOrEmail(ctx context.Context, username, email string) (*models.UserDao, error) {
+	queryBuilder := squirrel.Select("id", "username", "password", "email").From("users")
+
+	if username != "" && email == "" {
+		queryBuilder = queryBuilder.Where(squirrel.Eq{"username": username})
+	}
+	if email != "" && username == "" {
+		queryBuilder = queryBuilder.Where(squirrel.Eq{"email": email})
+	}
+	if username != "" && email != "" {
+		queryBuilder = queryBuilder.Where(squirrel.Or{squirrel.Eq{"username": username}, squirrel.Eq{"email": email}})
+	}
+
+	query, args, err := queryBuilder.PlaceholderFormat(squirrel.Dollar).ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var user models.UserDao
+	err = r.db.QueryRow(ctx, query, args...).
+		Scan(&user.Id, &user.Username, &user.Password, &user.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
